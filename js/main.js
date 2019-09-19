3a4bd0b848f6df82178e6683b2dcc307af204af9
lingjing0801 <dapaojing@gmail.com>
2019-09-19 21:04:56 +0800

    速度

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ $(document).ready(function () {
         // 取得目標標題的上方
         var top = $(link).offset().top;
         console.log(top);
+        // 取得目標的速度
         var speed =$(this).attr('data-speed');
         console.log(speed);
         $('html,body').animate({scrollTop:top},speed);

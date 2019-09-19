430401851e6dcdc520b65f2028cf88c8769c2e4d
lingjing0801 <dapaojing@gmail.com>
2019-09-19 20:04:27 +0800

    連結

--- js/main.js
+++ js/main.js
@@ -12,6 +12,15 @@ $(document).ready(function () {
     // scroll 捲動
     $(window).scroll(function () { 
         // 輸出訊息至控制台
-        console.log('捲動中...')
+        // console.log('捲動中...');
+        // scrollTop() 元素的上方位置
+        // console.log($(window).scrollTop());
+
+        // 如果螢幕高度 >=230 就顯示按鈕 否則就隱藏按鈕
+        if ($(window).scrollTop()>=230) {
+            $('#back-to-top').fadeIn(500);
+        } else {
+            $('#back-to-top').fadeOut(500);
+        }
     });
 });

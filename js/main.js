4e11446cded4365279f777aad28cd4c27a58e9ee
lingjing0801 <dapaojing@gmail.com>
2019-09-19 20:33:28 +0800

    捲動

--- js/main.js
+++ js/main.js
@@ -23,4 +23,10 @@ $(document).ready(function () {
             $('#back-to-top').fadeOut(500);
         }
     });
+    // --連結捲動到標題
+    $('.nav-link').click(function (e) { 
+        var link = $(this).attr('href');
+        console.log(link);
+        
+    });
 });
